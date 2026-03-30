ea7cf74036b62cc2bbba6925f4236dd4ce9d49c3
Esity <matthewdiverson@gmail.com>
2026-03-30 00:42:24 -0500

    add rubocop-legion 0.1.7, fix all offenses

--- lib/legion/extensions/redis.rb
+++ lib/legion/extensions/redis.rb
@@ -9,7 +9,7 @@ require 'legion/extensions/redis/client'
 module Legion
   module Extensions
     module Redis
-      extend Legion::Extensions::Core if Legion::Extensions.const_defined? :Core
+      extend Legion::Extensions::Core if Legion::Extensions.const_defined? :Core, false
     end
   end
 end
